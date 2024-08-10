52617ebcffdf37c34e3b9cfbdb624812c705f648
Pavel Bodrug <plkoij73@gmail.com>
2024-08-10 15:56:33 +0300

    update validation

--- src/validation/DataType.java
+++ src/validation/DataType.java
@@ -2,9 +2,10 @@ package validation;
 
 public enum DataType {
 
-    NAME("^[A-Z][a-z]{1,20}$"),
+    NAME("^[A-ZА-Я][a-zа-яA-ZА-Я]*(?:\\s[A-ZА-Я][a-zа-яA-ZА-Я]*)?$"),
     WORD("^[a-z]{1,20}$"),
-    NUMBER("^[0-9]{1,3}$");
+    NUMBER("^[0-9]{1,3}$"),
+    BOOL("^(true|false)");
 
     private final String pattern;
 
