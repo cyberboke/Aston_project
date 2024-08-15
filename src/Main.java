115c64611792e644e46c8ebf464da2f376be21fb
MariIva <maria.ivkina996@gmail.com>
2024-08-15 18:24:47 +0300

    main delete no use classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 
-import customClasses.Animal;
-import customClasses.Barrel;
-import customClasses.Person;
 import enums.TypeClass;
 import enums.*;
 import customClasses.factory.loader.LoaderFactory;
@@ -15,81 +12,71 @@ import java.util.*;
 
 public class Main {
     private static ListClasses listClasses;
+
+    private static TypeClass typeClass;
+    private static TypeAction typeAction;
     private static int count = 10;
+    private static TypeSort typeSort;
+    private static TypeChoice typeChoice;
 
     public static void main(String[] args) {
+        do {
+            typeClass = Menu.choosingClass();
+            listClasses = ListClasses.generateList(typeClass);
 
-        /*
-        TypeClass typeClass = Menu.choosingClass();
-        int size = Menu.choosingSize();
-        TypeLoad typeLoad = Menu.choosingLoad();
-        TypeAction typeAction = Menu.choosingAction();
-        Comparator comparator = Menu.choosingComparator(typeClass);
+            do {
+                TypeLoad typeLoad2 = Menu.choosingLoad();
+                count = Menu.choosingSize();
+                Actions.LOAD.addStrategy(listClasses.strategy::add, () -> {
+                    listClasses.list.clear();
+                    listClasses.list.addAll(
+                            LoaderFactory.getFactory(typeLoad2).load(typeClass, count)
+                    );
+                });
 
-        if (typeAction == TypeAction.SORT) {
-            TypeSort typeSort = Menu.choosingSort();
-        } else {
-            switch (typeClass) {
-                case ANIMAL: Animal animal = Menu.fillAnimal(comparator); break;
-                case PERSON: Person person = Menu.fillPerson(comparator); break;
-                case BARREL: Barrel barrel = Menu.fillBarrel(comparator); break;
-            }
-        }
+                do {
+                    typeAction = Menu.choosingAction();
+                    if (typeAction == TypeAction.SEARCH) {
+                        Actions.SORT.addStrategy(listClasses.strategy::add, () -> {
+                            TimSort.sort(listClasses.list);
+                        });
+                        Actions.SEARCH.addStrategy(listClasses.strategy::add, () -> {
+                            Object obj = Menu.createObject(typeClass);
+                            int index = BinarySearch.binarySearch(listClasses.list, (Comparable) obj);
+                            System.out.println("Object: " + obj);
+                            if (index == -1) {
+                                System.out.println("Not found"+"\n");
+                            } else {
+                                System.out.println("Found"+"\n");
+                            }
+                        });
+                    } else {
+                        typeSort = Menu.choosingSort();
+                        Actions.SORT.addStrategy(listClasses.strategy::add, () -> {
+                            Comparator comp = Menu.choosingComparator(typeClass);
+                            switch (typeSort) {
+                                case TIMSORT -> TimSort.sort(listClasses.list, comp);
+                                case EVENSORT -> EvenOddSort.sort(listClasses.list, true, comp);
+                                case ODDSORT -> EvenOddSort.sort(listClasses.list, false, comp);
+                            }
+                            listClasses.list.forEach(System.out::println);
+                            System.out.println();
+                        });
+                    }
+                    try {
+                        listClasses.executeAll();
+                    } catch (RuntimeException e) {
+                        System.err.println("An error has occurred. Please try again.");
+                    }
 
-        TypeChoice typeChoice = Menu.choosingChoice();
-        */
-
-
-        TypeClass type2 = TypeClass.ANIMAL; // иммитация выбора пользователя
-        listClasses = ListClasses.generateList(type2);
-
-        TypeLoad typeLoad2 = TypeLoad.LOAD_FILE; // иммитация выбора источника данных
-        count = 5;                                 // иммитация выбора количества объектов в массиве
-        Actions.LOAD.addStrategy(listClasses.strategy::add, ()-> {
-            listClasses.list.clear();
-            listClasses.list.addAll(
-                    LoaderFactory.getFactory(typeLoad2).load(type2, count)
-            );
-        });
-
-        TypeAction typeAction = TypeAction.SEARCH; // иммитация выбора
-        if(typeAction == TypeAction.SEARCH){
-            Object obj = Animal.builder(). // иммитация запроса объекта на поиск
-                    hair(false).
-                    kind("Cat").
-                    eyeColor(EyeColor.BLUE).
-                    build();
-            Actions.SORT.addStrategy(listClasses.strategy::add, ()->{
-                TimSort.sort(listClasses.list);
-            });
-            Actions.SEARCH.addStrategy(listClasses.strategy::add, ()->{
-                int index = BinarySearch.binarySearch(listClasses.list, (Comparable) obj);
-                System.out.println("Fount " + index);
-            });
-        }
-        else {
-            TypeSort typeSort = TypeSort.TIMSORT; // иммитация выбора сортировки
-            Actions.SORT.addStrategy(listClasses.strategy::add, ()->{
-                Comparator comp = null;         // иммитация выбора поля сортировки
-                switch (typeSort){
-                    case TIMSORT -> TimSort.sort(listClasses.list, comp);
-                    case EVENSORT -> EvenOddSort.sort(listClasses.list, true, comp);
-                    case ODDSORT -> EvenOddSort.sort(listClasses.list, false, comp);
-                }
-            });
-        }
-
-        try {
-            listClasses.executeAll();
-        }
-        catch (RuntimeException e){
-            System.err.println(e);
-        }
-
-        Actions.LOAD.removeStrategy(listClasses.strategy::remove); // Удаление действия из стратегии
-
-        listClasses.list.forEach(System.out::println);
-
+                    typeChoice = Menu.choosingChoice();
+                    if (typeChoice == TypeChoice.EXIT) return;
+                    Actions.SORT.removeStrategy(listClasses.strategy::remove);
+                    Actions.SEARCH.removeStrategy(listClasses.strategy::remove);
+                } while (typeChoice == TypeChoice.TYPE_ACTION);
+                Actions.LOAD.removeStrategy(listClasses.strategy::remove);
+            } while (typeChoice == TypeChoice.TYPE_LOAD);
+        } while (typeChoice == TypeChoice.TYPE_CLASS);
     }
 
 }
--- src/strategy/creationStrategy/CreationStrategy.java
+++
@@ -1,7 +0,0 @@
-package strategy.creationStrategy;
-
-import java.util.List;
-
-public interface CreationStrategy<T> {
-    List<T> create(int count);  // Метод для создания списка объектов
-}
